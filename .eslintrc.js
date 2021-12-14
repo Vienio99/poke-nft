4687cc6148bc7ab8a8695da99a75c8d5166dfc31
Vienio99 <kyve96@gmail.com>
2021-12-14 17:31:32 +0100

    configured eslint, prettier and couple minor fixes

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    quotes: [2, "double", { avoidEscape: true }],
+    // "import/prefer-default-export": "off",
+    "arrow-body-style": ["error", "always"],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+  },
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-center">Hello web3</h1>
+    </div>
+  );
+};
+
+export default App;

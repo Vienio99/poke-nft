15eb3304e2e16a89ffe127b9e563f844ecbb4329
Vienio99 <kyve96@gmail.com>
2021-12-14 14:05:47 +0100

    added truffle

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>
-        Goodbye world
+      <h1 className='text-3xl font-bold text-center'>
+        Hello web3
       </h1>
     </div>
   );
